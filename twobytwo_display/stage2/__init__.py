ae28741861284ff19f732f1fd2d2cb92a027b134
Bruno Gelli <62032408+BrunoGelli@users.noreply.github.com>
2026-05-20 17:12:35 -0700

    Add minimal Stage 2 pipeline skeleton and DBSCAN producer

---
+++ twobytwo_display/stage2/__init__.py
@@ -0,0 +1,9 @@
+from .pipeline import ParamSpec, StepResult, CutStep, Stage2Pipeline, CutRegistry
+
+__all__ = [
+    "ParamSpec",
+    "StepResult",
+    "CutStep",
+    "Stage2Pipeline",
+    "CutRegistry",
+]
